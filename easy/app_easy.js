7c1a3df927bf2a1fa5c1e321898558cc6c2dc19c
javi-dh <javier@digitalhouse.com>
2019-11-01 15:44:13 -0300

    Add app_easy.js

--- easy/app_easy.js
+++ easy/app_easy.js
@@ -195,4 +195,32 @@ const theaters = [
 ];
 
 // Servidor
-http.createServer().listen();
+http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+	
+	// Route System
+	switch (req.url) {
+		// Home
+		case '/':
+			res.end('Home');
+			break;
+		// En cartelera
+		case '/en-cartelera':
+			res.end('En cartelera');
+			break;
+		case '/mas-votadas':
+			res.end('Más Votadas');
+			break;
+		case '/sucursales':
+			res.end('Sucursales');
+			break;
+		case '/contacto':
+			res.end('Contacto');
+			break;
+		case '/preguntas-frecuentes':
+			res.end('Preguntas Frecuentes');
+			break;
+		default:
+			res.end('404 not found')
+	}
+}).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
